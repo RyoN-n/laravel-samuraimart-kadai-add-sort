6ef093ebc32c4c884f56a39476d4b87a1217cb6c
RyoN-n <147308889+RyoN-n@users.noreply.github.com>
2024-03-09 13:31:07 +0900

    Add files via upload

---
+++ laravel-samuraimart/routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CartController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::post('reviews', [ReviewController::class, 'store'])->name('reviews.store');
+
+// Route::resource('products', ProductController::class)->middleware(['auth', 'verified']);
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('products', ProductController::class);
+
+    Route::post('reviews', [ReviewController::class, 'store'])->name('reviews.store');
+
+    Route::post('favorites/{product_id}', [FavoriteController::class, 'store'])->name('favorites.store');
+    Route::delete('favorites/{product_id}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('users/mypage', 'mypage')->name('mypage');
+        Route::get('users/mypage/edit', 'edit')->name('mypage.edit');
+        Route::put('users/mypage', 'update')->name('mypage.update');
+        Route::get('users/mypage/password/edit', 'edit_password')->name('mypage.edit_password');
+        Route::put('users/mypage/password', 'update_password')->name('mypage.update_password');
+        Route::get('users/mypage/favorite', 'favorite')->name('mypage.favorite');
+    });
+
+    Route::controller(CartController::class)->group(function () {
+        Route::get('users/carts', 'index')->name('carts.index');
+        Route::post('users/carts', 'store')->name('carts.store');
+        Route::delete('users/carts', 'destroy')->name('carts.destroy');
+    });
+});
