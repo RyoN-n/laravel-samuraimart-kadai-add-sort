63d40efcfc675a513ebc598bc9c4c808f4a60337
RyoN-n <147308889+RyoN-n@users.noreply.github.com>
2024-03-09 13:32:24 +0900

    Add files via upload

---
+++ laravel-samuraimart/storage/framework/views/3ad2f077867a8095cd53e7098db2cf7464a432b9.php
@@ -0,0 +1,170 @@
+
+ 
+ <?php $__env->startSection('content'); ?>
+ <div class="container">
+     <div class="row justify-content-center">
+         <div class="col-md-5">
+             <span>
+                 <a href="<?php echo e(route('mypage')); ?>">マイページ</a> > 会員情報の編集
+             </span>
+ 
+             <h1 class="mt-3 mb-3">会員情報の編集</h1>
+             <hr>
+ 
+             <form method="POST" action="<?php echo e(route('mypage')); ?>">
+                 <?php echo csrf_field(); ?>
+                 <input type="hidden" name="_method" value="PUT">
+                 <div class="form-group">
+                     <div class="d-flex justify-content-between">
+                         <label for="name" class="text-md-left samuraimart-edit-user-info-label">氏名</label>
+                     </div>
+                     <div class="collapse show editUserName">
+                         <input id="name" type="text" class="form-control <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="name" value="<?php echo e($user->name); ?>" required autocomplete="name" autofocus placeholder="侍 太郎">
+                         <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                         <span class="invalid-feedback" role="alert">
+                             <strong>氏名を入力してください</strong>
+                         </span>
+                         <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                     </div>
+                 </div>
+                 <br>
+ 
+                 <div class="form-group">
+                     <div class="d-flex justify-content-between">
+                         <label for="email" class="text-md-left samuraimart-edit-user-info-label">メールアドレス</label>
+                     </div>
+                     <div class="collapse show editUserMail">
+                         <input id="email" type="text" class="form-control <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="email" value="<?php echo e($user->email); ?>" required autocomplete="email" autofocus placeholder="samurai@samurai.com">
+                         <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                         <span class="invalid-feedback" role="alert">
+                             <strong>メールアドレスを入力してください</strong>
+                         </span>
+                         <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                     </div>
+                 </div>
+                 <br>
+ 
+                 <div class="form-group">
+                     <div class="d-flex justify-content-between">
+                         <label for="postal_code" class="text-md-left samuraimart-edit-user-info-label">郵便番号</label>
+                     </div>
+                     <div class="collapse show editUserPhone">
+                         <input id="postal_code" type="text" class="form-control <?php $__errorArgs = ['postal_code'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="postal_code" value="<?php echo e($user->postal_code); ?>" required autocomplete="postal_code" autofocus placeholder="XXX-XXXX">
+                         <?php $__errorArgs = ['postal_code'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                         <span class="invalid-feedback" role="alert">
+                             <strong>郵便番号を入力してください</strong>
+                         </span>
+                         <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                     </div>
+                 </div>
+                 <br>
+ 
+                 <div class="form-group">
+                     <div class="d-flex justify-content-between">
+                         <label for="address" class="text-md-left samuraimart-edit-user-info-label">住所</label>
+                     </div>
+                     <div class="collapse show editUserPhone">
+                         <input id="address" type="text" class="form-control <?php $__errorArgs = ['address'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="address" value="<?php echo e($user->address); ?>" required autocomplete="address" autofocus placeholder="東京都渋谷区道玄坂X-X-X">
+                         <?php $__errorArgs = ['address'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                         <span class="invalid-feedback" role="alert">
+                             <strong>住所を入力してください</strong>
+                         </span>
+                         <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                     </div>
+                 </div>
+                 <br>
+ 
+                 <div class="form-group">
+                     <div class="d-flex justify-content-between">
+                         <label for="phone" class="text-md-left samuraimart-edit-user-info-label">電話番号</label>
+                     </div>
+                     <div class="collapse show editUserPhone">
+                         <input id="phone" type="text" class="form-control <?php $__errorArgs = ['phone'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="phone" value="<?php echo e($user->phone); ?>" required autocomplete="phone" autofocus placeholder="XXX-XXXX-XXXX">
+                         <?php $__errorArgs = ['phone'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                         <span class="invalid-feedback" role="alert">
+                             <strong>電話番号を入力してください</strong>
+                         </span>
+                         <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                     </div>
+                 </div>
+ 
+                 <hr>
+                 <button type="submit" class="btn samuraimart-submit-button mt-3 w-25">
+                     保存
+                 </button>
+             </form>
+         </div>
+     </div>
+ </div>
+ <?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-samuraimart\resources\views/users/edit.blade.php ENDPATH**/ ?>
---
+++ laravel-samuraimart/storage/framework/views/ae940cbbb3d8d277ee8dae0586209a1cf62bed59.php
@@ -0,0 +1,74 @@
+
+ 
+ <?php $__env->startSection('content'); ?>
+ <div class="container d-flex justify-content-center mt-3">
+     <div class="w-75">
+         <h1>ショッピングカート</h1>
+ 
+         <div class="row">
+             <div class="offset-8 col-4">
+                 <div class="row">
+                     <div class="col-6">
+                         <h2>数量</h2>
+                     </div>
+                     <div class="col-6">
+                         <h2>合計</h2>
+                     </div>
+                 </div>
+             </div>
+         </div>
+ 
+         <hr>
+ 
+         <div class="row">
+             <?php $__currentLoopData = $cart; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+             <div class="col-md-2 mt-2">
+                 <a href="<?php echo e(route('products.show', $product->id)); ?>">
+                     <img src="<?php echo e(asset('img/dummy.png')); ?>" class="img-fluid w-100">
+                 </a>
+             </div>
+             <div class="col-md-6 mt-4">
+                 <h3 class="mt-4"><?php echo e($product->name); ?></h3>
+             </div>
+             <div class="col-md-2">
+                 <h3 class="w-100 mt-4"><?php echo e($product->qty); ?></h3>
+             </div>
+             <div class="col-md-2">
+                 <h3 class="w-100 mt-4">￥<?php echo e($product->qty * $product->price); ?></h3>
+             </div>
+             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+         </div>
+ 
+         <hr>
+ 
+         <div class="offset-8 col-4">
+             <div class="row">
+                 <div class="col-6">
+                     <h2>合計</h2>
+                 </div>
+                 <div class="col-6">
+                     <h2>￥<?php echo e($total); ?></h2>
+                 </div>
+                 <div class="col-12 d-flex justify-content-end">
+                     表示価格は税込みです
+                 </div>
+             </div>
+         </div>
+
+         <form method="post" action="<?php echo e(route('carts.destroy')); ?>" class="d-flex justify-content-end mt-3">
+             <?php echo csrf_field(); ?>
+             <input type="hidden" name="_method" value="DELETE">
+             <a href="" class="btn samuraimart-favorite-button border-dark text-dark mr-3">
+                 買い物を続ける
+             </a>
+             <?php if($total > 0): ?>
+             <button type="submit" class="btn samuraimart-submit-button">購入を確定する</button>
+             <?php else: ?>
+             <button type="submit" class="btn samuraimart-submit-button disabled">購入を確定する</button>
+             <?php endif; ?>
+         </form>
+     </div>
+ </div>
+ <?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-samuraimart\resources\views/carts/index.blade.php ENDPATH**/ ?>
