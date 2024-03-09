cb63f725386f9e8df9295618d96346023cfca6e4
RyoN-n <147308889+RyoN-n@users.noreply.github.com>
2024-03-09 13:28:29 +0900

    Add files via upload

---
+++ laravel-samuraimart/app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Overtrue\LaravelFavorite\Traits\Favoriteable;
+use Kyslik\ColumnSortable\Sortable;
+
+
+
+class Product extends Model
+{
+    use HasFactory, Sortable;
+
+    protected $sortable = [
+        'name', //商品名でソート
+        'price', //値段でソート
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reviews() {
+        return $this->hasMany(Review::class);
+    }
+
+    public function favorited_users() {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+}
